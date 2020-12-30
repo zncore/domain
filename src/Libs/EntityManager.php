42c1f88c2332fb6385b7dd1c247eee58ecc33f25
Vitaliy <theyamshikov@yandex.ru>
2020-12-30 22:41:28 +0600

    upd

---
+++ src/Libs/EntityManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ZnCore\Domain\Libs;
+
+use Illuminate\Support\Collection;
+use Psr\Container\ContainerInterface;
+use ZnCore\Domain\Helpers\EntityHelper;
+
+class EntityManager
+{
+
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function createEntity(string $entityClassName, $attributes = []): object
+    {
+        $entityInstance = $this->container->get($entityClassName);
+        if ($attributes) {
+            EntityHelper::setAttributes($entityInstance, $attributes);
+        }
+        return $entityInstance;
+    }
+
+    public function createEntityCollection(string $entityClassName, array $items): Collection
+    {
+        $collection = new Collection();
+        foreach ($items as $item) {
+            $entityInstance = $this->createEntity($entityClassName, $item);
+            $collection->add($entityInstance);
+        }
+        return $collection;
+    }
+}
