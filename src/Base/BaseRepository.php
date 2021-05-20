1947ebf4b8790ed7aeb7de65c4f2655f3b9507ec
Vitaliy <theyamshikov@yandex.ru>
2021-05-20 10:35:06 +0600

    upd

--- src/Base/BaseRepository.php
+++ src/Base/BaseRepository.php
@@ -2,7 +2,10 @@
 
 namespace ZnCore\Domain\Base;
 
+use ZnCore\Base\Helpers\DeprecateHelper;
 use ZnCore\Domain\Interfaces\GetEntityClassInterface;
+
+DeprecateHelper::hardThrow();
 
 abstract class BaseRepository implements GetEntityClassInterface
 {
--- src/Base/Repositories/BaseFileCrudRepository.php
+++ src/Base/Repositories/BaseFileCrudRepository.php
@@ -2,17 +2,52 @@
 
 namespace ZnCore\Domain\Base\Repositories;
 
+use ZnCore\Base\Exceptions\NotImplementedMethodException;
+use ZnCore\Base\Legacy\Yii\Helpers\FileHelper;
+use ZnCore\Base\Libs\DotEnv\DotEnv;
 use ZnCore\Base\Libs\Store\StoreFile;
+use ZnCore\Domain\Interfaces\Repository\CrudRepositoryInterface;
+use ZnCore\Domain\Traits\Repository\ArrayCrudRepositoryTrait;
 
-abstract class BaseFileCrudRepository extends BaseArrayCrudRepository
+abstract class BaseFileCrudRepository extends BaseFileRepository implements CrudRepositoryInterface
 {
 
-    abstract public function fileName(): string;
+    use ArrayCrudRepositoryTrait;
+
+    public function tableName(): string
+    {
+        throw new NotImplementedMethodException('Not Implemented Method "tableName"');
+    }
+
+    public function directory(): string
+    {
+        return DotEnv::get('FILE_DB_DIRECTORY');
+    }
+
+    public function fileExt(): string
+    {
+        return 'php';
+    }
+
+    public function relations()
+    {
+        return [];
+    }
+
+    public function fileName(): string
+    {
+        $tableName = $this->tableName();
+        $root = FileHelper::rootPath();
+        $directory = $this->directory();
+        $ext = $this->fileExt();
+        $path = "$root/$directory/$tableName.$ext";
+        return $path;
+    }
 
     protected function getItems(): array
     {
         $store = new StoreFile($this->fileName());
-        return $store->load();
+        return $store->load() ?: [];
     }
 
     protected function setItems(array $items)
