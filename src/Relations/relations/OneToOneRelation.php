a7f526547a15486cf7799b32e3d7311c2f055511
Vitaliy <theyamshikov@yandex.ru>
2021-08-31 16:48:45 +0600

    upd

--- src/Relations/relations/OneToOneRelation.php
+++ src/Relations/relations/OneToOneRelation.php
@@ -2,9 +2,9 @@
 
 namespace ZnCore\Domain\Relations\relations;
 
+use Illuminate\Support\Collection;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use ZnCore\Base\Legacy\Yii\Helpers\ArrayHelper;
-use ZnCore\Base\Libs\ArrayTools\Helpers\Collection;
 use ZnCore\Domain\Helpers\EntityHelper;
 use ZnCore\Domain\Libs\Query;
 use ZnCore\Domain\Relations\interfaces\CrudRepositoryInterface;
