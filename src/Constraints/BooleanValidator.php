07d45d431e58de74d3bb81b5a601fcb0f5ee7bfb
Vitaliy <theyamshikov@yandex.ru>
2021-09-06 16:37:15 +0600

    upd

---
+++ src/Constraints/BooleanValidator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ZnCore\Domain\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class BooleanValidator extends ConstraintValidator
+{
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof Boolean) {
+            throw new UnexpectedTypeException($constraint, Boolean::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) to take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_bool($value)) {
+            // throw this exception if your validator cannot handle the passed type so that it can be marked as invalid
+            throw new UnexpectedValueException($value, 'boolean');
+
+            // separate multiple types using pipes
+            // throw new UnexpectedValueException($value, 'string|int');
+        }
+    }
+}
