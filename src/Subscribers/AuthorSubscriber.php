f3dff5216af45a796e924744c8b86385f22e2cb4
Vitaliy <theyamshikov@yandex.ru>
2021-09-15 11:46:28 +0600

    upd

---
+++ src/Subscribers/AuthorSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ZnCore\Domain\Subscribers;
+
+use App\News\Domain\Entities\CommentEntity;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ZnBundle\User\Domain\Exceptions\UnauthorizedException;
+use ZnBundle\User\Domain\Interfaces\Services\AuthServiceInterface;
+use ZnCore\Domain\Enums\EventEnum;
+use ZnCore\Domain\Events\EntityEvent;
+use ZnCore\Domain\Helpers\EntityHelper;
+
+class AuthorSubscriber implements EventSubscriberInterface
+{
+
+    private $authService;
+    private $attribute;
+
+    public function __construct(
+        AuthServiceInterface $authService
+    )
+    {
+        $this->authService = $authService;
+    }
+
+    public function setAttribute(string $attribute): void
+    {
+        $this->attribute = $attribute;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            EventEnum::BEFORE_CREATE_ENTITY => 'onCreateComment'
+        ];
+    }
+
+    public function onCreateComment(EntityEvent $event)
+    {
+        /** @var CommentEntity $entity */
+        $entity = $event->getEntity();
+
+        try {
+            $identityId = $this->authService->getIdentity()->getId();
+            EntityHelper::setAttribute($entity, $this->attribute, $identityId);
+        } catch (UnauthorizedException $e) {
+        }
+    }
+}
