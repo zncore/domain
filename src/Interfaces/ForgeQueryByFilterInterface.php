029464e28b76374af966284c8d22335f0c691161
Vitaliy <theyamshikov@yandex.ru>
2021-10-13 10:23:30 +0600

    upd

--- src/Interfaces/ForgeQueryByFilterInterface.php
+++ src/Interfaces/ForgeQueryByFilterInterface.php
@@ -2,10 +2,13 @@
 
 namespace ZnCore\Domain\Interfaces;
 
-use ZnCore\Domain\Libs\Query;
-
-interface ForgeQueryByFilterInterface
+/**
+ * Interface ForgeQueryByFilterInterface
+ * @package ZnCore\Domain\Interfaces
+ * @deprecated 
+ * @see \ZnCore\Contract\Domain\Interfaces\ForgeQueryByFilterInterface
+ */
+interface ForgeQueryByFilterInterface extends \ZnCore\Contract\Domain\Interfaces\ForgeQueryByFilterInterface
 {
-
-    public function forgeQueryByFilter(object $filterModel, Query $query);
+    
 }
