35d04c6845e921944ca97d57a35d1e15bd610a10
Vitaliy <theyamshikov@yandex.ru>
2021-12-17 20:38:21 +0600

    upd

---
+++ src/Constraints/ArrValidator.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ZnCore\Domain\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class ArrValidator extends BaseValidator
+{
+
+    protected $constraintClass = Arr::class;
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (!is_array($value)) {
+            // throw this exception if your validator cannot handle the passed type so that it can be marked as invalid
+            throw new UnexpectedValueException($value, 'array');
+
+            // separate multiple types using pipes
+            // throw new UnexpectedValueException($value, 'string|int');
+        }
+    }
+}
