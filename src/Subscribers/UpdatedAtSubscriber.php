6c6805a8acdb53c882512bbeb90e82206f4f05bc
ZnDeveloper <zndev@ya.ru>
2022-01-22 15:04:26 +0600

    upd

---
+++ src/Subscribers/UpdatedAtSubscriber.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ZnCore\Domain\Subscribers;
+
+use App\News\Domain\Entities\CommentEntity;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ZnCore\Domain\Enums\EventEnum;
+use ZnCore\Domain\Events\EntityEvent;
+
+class UpdatedAtSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            EventEnum::BEFORE_CREATE_ENTITY => 'onBeforePersist',
+            EventEnum::BEFORE_UPDATE_ENTITY => 'onBeforePersist',
+        ];
+    }
+
+    public function onBeforePersist(EntityEvent $event)
+    {
+        $entity = $event->getEntity();
+//        dd($entity);
+        $entity->setUpdatedAt(new \DateTime());
+    }
+}
