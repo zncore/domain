a363d6af573ca2267ec8764854941b2c1f1ed76a
Vitaliy <theyamshikov@yandex.ru>
2022-06-01 17:44:40 +0600

    upd

--- src/Constraints/ArrValidator.php
+++ src/Constraints/ArrValidator.php
@@ -14,7 +14,7 @@ class ArrValidator extends BaseValidator
 
     public function validate($value, Constraint $constraint)
     {
-        if (!is_array($value)) {
+        if ($value && !is_array($value)) {
             // throw this exception if your validator cannot handle the passed type so that it can be marked as invalid
             throw new UnexpectedValueException($value, 'array');
 
