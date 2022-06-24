e887c32f859f6e4d79cba0a63b9307aae7379359
Vitaliy <theyamshikov@yandex.ru>
2022-06-24 17:22:03 +0600

    upd

--- src/Repository/Traits/RepositoryFindOneTrait.php
+++ src/Repository/Traits/RepositoryFindOneTrait.php
@@ -3,10 +3,12 @@
 namespace ZnCore\Domain\Repository\Traits;
 
 use ZnCore\Base\I18Next\Facades\I18Next;
+use ZnCore\Base\Text\Helpers\Inflector;
 use ZnCore\Contract\Common\Exceptions\InvalidMethodParameterException;
 use ZnCore\Domain\Domain\Enums\EventEnum;
 use ZnCore\Domain\Entity\Exceptions\AlreadyExistsException;
 use ZnCore\Domain\Entity\Exceptions\NotFoundException;
+use ZnCore\Domain\Entity\Helpers\EntityHelper;
 use ZnCore\Domain\Entity\Interfaces\EntityIdInterface;
 use ZnCore\Domain\Entity\Interfaces\UniqueInterface;
 use ZnCore\Domain\Query\Entities\Query;
@@ -53,18 +55,36 @@ trait RepositoryFindOneTrait
         }
     }
 
+
     public function oneByUnique(UniqueInterface $entity): EntityIdInterface
     {
         $unique = $entity->unique();
         if (!empty($unique)) {
             foreach ($unique as $uniqueConfig) {
-                $query = RepositoryUniqueHelper::buildQuery($entity, $uniqueConfig);
-                $all = $this->all($query);
-                if ($all->count() > 0) {
-                    return $all->first();
+                $oneEntity = $this->oneByUniqueGroup($entity, $uniqueConfig);
+                if($oneEntity) {
+                    return $oneEntity;
                 }
             }
         }
         throw new NotFoundException();
+    }
+
+    private function oneByUniqueGroup(UniqueInterface $entity, $uniqueConfig): ?EntityIdInterface
+    {
+        $isBreak = false;
+        $query = new Query();
+        foreach ($uniqueConfig as $uniqueName) {
+            $value = EntityHelper::getValue($entity, $uniqueName);
+            if($value === null) {
+                return null;
+            }
+            $query->where(Inflector::underscore($uniqueName), $value);
+        }
+        $all = $this->all($query);
+        if ($all->count() > 0) {
+            return $all->first();
+        }
+        return null;
     }
 }
