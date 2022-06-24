ef1e4f265835073233890bc92c8ae72f5f1a1a7e
Vitaliy <theyamshikov@yandex.ru>
2022-06-24 16:46:26 +0600

    upd

--- src/Repository/Traits/RepositoryRelationTrait.php
+++ src/Repository/Traits/RepositoryRelationTrait.php
@@ -3,6 +3,8 @@
 namespace ZnCore\Domain\Repository\Traits;
 
 use Illuminate\Support\Collection;
+use ZnCore\Domain\Query\Entities\Query;
+use ZnCore\Domain\Relation\Libs\RelationLoader;
 
 trait RepositoryRelationTrait
 {
@@ -13,9 +15,29 @@ trait RepositoryRelationTrait
 
     public function loadRelations(Collection $collection, array $with)
     {
+//        if (method_exists($this, 'relations')) {
+            $relations = $this->relations();
+            if(empty($relations)) {
+                return;
+            }
+            $query = new Query();
+            $query->with($with);
+            $relationLoader = new RelationLoader();
+            $relationLoader->setRelations($relations);
+            $relationLoader->setRepository($this);
+            $relationLoader->loadRelations($collection, $query);
+//        }
+    }
+
+    public function loadRelationsByQuery(Collection $collection, Query $query) {
+        $this->loadRelations($collection, $query->getWith() ?: []);
+    }
+
+    /*public function loadRelations(Collection $collection, array $with)
+    {
         $query = $this->forgeQuery();
         $query->with($with);
         $queryFilter = $this->queryFilterInstance($query);
         $queryFilter->loadRelations($collection);
-    }
+    }*/
 }
