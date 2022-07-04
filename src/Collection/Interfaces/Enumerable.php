d82050aa30ec05e00277f646829bf7afd557bc30
ZnDeveloper <zndev@ya.ru>
2022-07-05 00:13:16 +0600

    upd

--- src/Collection/Interfaces/Enumerable.php
+++ src/Collection/Interfaces/Enumerable.php
@@ -3,7 +3,6 @@
 namespace ZnCore\Domain\Collection\Interfaces;
 
 interface Enumerable extends \Doctrine\Common\Collections\Collection
-//interface Enumerable extends \Illuminate\Support\Enumerable
 {
 
 }
